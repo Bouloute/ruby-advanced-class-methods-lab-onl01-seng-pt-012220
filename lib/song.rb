bc1b5f68a5c745ddc06a9b3c5768265bf85963bd
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-01-30 22:54:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,6 +21,13 @@ class Song
   def self.new_by_name(song_name)
     song = self.new
     song.name = song_name
+     #song.save
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.new
+    song.name = song_name
     song.save
     song
   end
